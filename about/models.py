46ec3dd1af41a8b0e9ad77758dd3e5a00523c97e
m.hvozdesky <m.hvozdesky@gmail.com>
2020-10-27 18:22:20 +0200

    added meta class to models

--- about/models.py
+++ about/models.py
@@ -11,6 +11,9 @@ class MainSlider(models.Model):
     picture_1280_1024 = models.ImageField(upload_to='images/slider')
     picture_2560_1080 = models.ImageField(upload_to='images/slider')
     
+    class Meta:
+        verbose_name_plural = "Main Slider"
+    
 class Automobiles(models.Model):
     #the model presents Automobiles
     
@@ -18,6 +21,9 @@ class Automobiles(models.Model):
     picture_770_340 = models.ImageField(upload_to='images/automobiles')
     availability_hybrid = models.BooleanField(default=False)
     price_starts = models.FloatField()
+    
+    class Meta:
+        verbose_name_plural = "Automobiles"    
 
 class News(models.Model):
     #the model presents News
@@ -25,6 +31,9 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     publication_date = models.DateField(default=datetime.date.today)
+    
+    class Meta:
+        verbose_name_plural = "News"    
 
 class Promotion(models.Model):
     #the model presents Automobiles
@@ -32,3 +41,6 @@ class Promotion(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     publication_date = models.DateField(default=datetime.date.today)
+    
+    class Meta:
+        verbose_name_plural = "Promotion"      
