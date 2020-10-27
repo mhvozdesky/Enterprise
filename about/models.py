678ea977cca3474b6ce3cf9e66f99e83bd748421
m.hvozdesky <m.hvozdesky@gmail.com>
2020-10-27 17:21:24 +0200

    models.py

--- about/models.py
+++ about/models.py
@@ -1,3 +1,34 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class MainSlider(models.Model):
+    #the model presents the main slider
+    
+    slide_number = models.IntegerField(min_value=1)
+    picture_1920_1080 = models.ImageField(upload_to='images/slider')
+    picture_1024_768 = models.ImageField(upload_to='images/slider')
+    picture_1280_1024 = models.ImageField(upload_to='images/slider')
+    picture_2560_1080 = models.ImageField(upload_to='images/slider')
+    
+class Automobiles(models.Model):
+    #the model presents Automobiles
+    
+    title = models.CharField(max_length=200)
+    picture_770_340 = models.ImageField(upload_to='images/automobiles')
+    availability_hybrid = models.BooleanField(default=False)
+    price_starts = models.FloatField()
+
+class News(models.Model):
+    #the model presents News
+    
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    publication_date = models.DateField(default=datetime.date.today)
+
+class Promotion(models.Model):
+    #the model presents Automobiles
+    
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    publication_date = models.DateField(default=datetime.date.today)
