9b40642f3c3dedafb756b0a33fbc0641d37997cc
m.hvozdesky <m.hvozdesky@gmail.com>
2020-11-09 16:30:06 +0200

    editing the visualizer

--- about/views.py
+++ about/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from about.models import MainSlider, Automobiles, News, Promotion
 
 def index(request):
+    count_slides = MainSlider.objects.all().count()
+    list_slides = MainSlider.objects.all()
+    
+    list_automobiles = Automobiles.objects.all()
+    
+    context = {
+        'count_slides': count_slides,
+        'list_slides': list_slides,
+        'list_automobiles': list_automobiles,
+    }
+    
     return render(request, r'index\index.html')
