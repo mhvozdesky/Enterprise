7351ca28d3546f948761b4525e48d1f7353cc1b8
m.hvozdesky <m.hvozdesky@gmail.com>
2020-11-09 17:15:38 +0200

    filled in the visualizer. So far, everything works.

--- about/views.py
+++ about/views.py
@@ -5,13 +5,16 @@ def index(request):
     count_slides = MainSlider.objects.all().count()
     list_slides = MainSlider.objects.all()
     list_automobiles = Automobiles.objects.all()
-    #last_News
+    last_news = News.objects.all()[0]
+    last_promotion = Promotion.objects.all()[0]
     
     context = {
         'count_slides': count_slides,
         'list_slides': list_slides,
         'list_automobiles': list_automobiles,
+        'last_news': last_news,
+        'last_promotion': last_promotion,
     }
     
 
-    return render(request, r'index\index.html')
+    return render(request, r'index\index.html', context=context)
