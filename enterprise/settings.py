76d0d411ffd331fd9ed60f90ad3c51e555215a9e
m.hvozdesky <m.hvozdesky@gmail.com>
2021-01-08 13:19:27 +0200

    added to header UK and RU

--- enterprise/settings.py
+++ enterprise/settings.py
@@ -127,6 +127,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CKEDITOR_UPLOAD_PATH = "uploads/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 CKEDITOR_CONFIGS = { 
     'default': { 
