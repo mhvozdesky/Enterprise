f9faaeb72ee7498979d77fd862ad77648c4ba923
m.hvozdesky <m.hvozdesky@gmail.com>
2021-01-23 14:24:28 +0200

    Home page switches to another language

--- about/views.py
+++ about/views.py
@@ -1,7 +1,15 @@
+import re
 from django.shortcuts import render
 from about.models import MainSlider, Automobiles, News, Promotion
 
 def index(request):
+    if len(re.findall(r'^/ru', request.path)) > 0:
+        lang = 'ru'
+        path_other_lang = request.path[3:] + r'/'
+    else:
+        lang = 'uk'
+        path_other_lang = request.path + 'ru'
+        
     count_slides = MainSlider.objects.all().count()
     list_slides = MainSlider.objects.all()
     list_automobiles = Automobiles.objects.all()
@@ -9,6 +17,8 @@ def index(request):
     last_promotion = Promotion.objects.all()[0]
     
     context = {
+        'lang': lang,
+        'path_other_lang': path_other_lang,
         'count_slides': count_slides,
         'list_slides': list_slides,
         'list_automobiles': list_automobiles,
