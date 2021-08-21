600fdc9055f8fa379bef3afabfd2c494233a5dbe
m.hvozdesky <m.hvozdesky@gmail.com>
2021-08-21 11:36:39 +0300

    the menu button is activated by pressing

--- about/static/js/index/app.js
+++ about/static/js/index/app.js
@@ -1,3 +1,26 @@
 function show_menu_modile(x){
-  console.log(x);
+  var a = document.querySelector('.header-menu');
+  a.classList.toggle("activ");
 }
+
+function ClickListener(event){
+
+  edit_menu_button(event);
+
+}
+
+function edit_menu_button(e){
+  
+  var header_menu = document.querySelector('.header-menu');
+  var mobile_menu_button = document.querySelector('.title-menu-modile a');
+
+  if (!header_menu.contains(e.target) && !mobile_menu_button.contains(e.target)){
+    header_menu.classList.toggle("activ");
+  }
+
+}
+
+
+
+
+document.addEventListener('click', ClickListener);
