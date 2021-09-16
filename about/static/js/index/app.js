9be7319b79e79e95fa7a0ed794b274df2662a995
m.hvozdesky <m.hvozdesky@gmail.com>
2021-09-16 11:47:59 +0300

    finished the first section, fully adapted

--- about/static/js/index/app.js
+++ about/static/js/index/app.js
@@ -21,7 +21,40 @@ function edit_menu_button(e){
 
 }
 
+function pos_slider_description(){
+	var header = document.querySelector('.header-conteiner');
+	var slider_description = document.querySelector('.slider-description');
+	slider_description.style.transform = "translate(0px, 0px)"; // в исходное положение
+
+	//получить низ хедера и верх slider_description
+	var header_bottom = header.getBoundingClientRect().bottom;
+	var slider_description_top = slider_description.getBoundingClientRect().top;
+
+	dif = header_bottom - slider_description_top; // на сколько slider_description вылазит за хедер
+
+	if (dif > 0){
+		slider_description.style.transform = "translate(0px, " + String(dif) + "px)";
+	} else{
+		slider_description.style.transform = "translate(0px, 0px)"; // в исходное положение
+	}
+
+}
+
+function resizeListener(){
+	pos_slider_description();
+}
+
 
 
 
 document.addEventListener('click', ClickListener);
+
+window.onload = function() {
+  pos_slider_description();
+};
+
+window.addEventListener('resize', resizeListener);
+
+// window.addEventListener(`resize`, event => {
+//   pos_slider_description();
+// }, false);
