95281bb4972347f33cb51dab6c88014557294aaf
m.hvozdesky <m.hvozdesky@gmail.com>
2021-10-23 14:08:17 +0300

    made a basic template

--- models/urls.py
+++ models/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path
 from django.views.generic import RedirectView
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'), 
     #re_path(r'^[a-zA-Z]{2}/', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #urlpatterns = [
     #path('', views.index, name='index'), 
--- models/views.py
+++ models/views.py
@@ -2,6 +2,7 @@ import re
 from django.shortcuts import render
 from local_languages import lang_file
 
+
 def index(request):
     context = {}
     template = r'models_base.html'
